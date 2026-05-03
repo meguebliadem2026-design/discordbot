5ea99b3c27555a2122890234ec3de9b7688f50f2
meguebliadem2026-design <meguebli.adem2026@gmail.com>
2026-05-03 18:16:36 +0200

    Update keepAlive.js

--- keepAlive.js
+++ keepAlive.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 
-// Route principale (keep alive)
 app.get("/", (req, res) => {
   res.status(200).send("Bot is alive ✅");
 });
 
-// Port Render / Replit / local
 const PORT = process.env.PORT || 3000;
 
-// Lancement du serveur
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Serveur actif sur le port ${PORT}`);
+  console.log(`🌐 KeepAlive actif sur port ${PORT}`);
 });
